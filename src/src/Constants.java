cd4f7b181e49e8b229e05cbee9f1cd208e4a0f7f
jebryce <116392465+jebryce@users.noreply.github.com>
2023-03-24 18:57:29 -1000

    basic window appears

---
+++ src/src/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    public  static final int  UPDATES_PER_SECOND = 60;
+    public  static final int  TILE_SIZE          = 64;
+    public  static final int  MAX_SCREEN_ROW     = 9;
+    public  static final int  MAX_SCREEN_COL     = 9;
+    public  static final int  SCREEN_HEIGHT      = TILE_SIZE * MAX_SCREEN_ROW;
+    public  static final int  SCREEN_WIDTH       = TILE_SIZE * MAX_SCREEN_COL;
+    public  static final long NANO_SEC_PER_SEC   = 1_000_000_000L;
+}
---
+++ src/src/GamePanel.java
@@ -0,0 +1,65 @@
+
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+    private       Thread        gameThread;
+
+    public GamePanel() {
+        this.setPreferredSize( new Dimension( Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT ) );
+        this.setBackground( Color.BLACK );
+        this.setDoubleBuffered( true );
+        //this.addKeyListener( keyH );
+        this.setFocusTraversalKeysEnabled( false ); // can receive tab inputs
+        this.setFocusable( true );
+    }
+
+    @Override
+    public void run() {
+        final long updateFPSInterval  = Constants.NANO_SEC_PER_SEC / 5;
+        final long updateGameInterval = Constants.NANO_SEC_PER_SEC / Constants.UPDATES_PER_SECOND;
+        long       lastUpdateTime     = 0;
+        long       currentTime;
+        long       lastFPSTime        = 0;
+        long       numFrames          = 0;
+        double     FPS;
+
+        // main game loop
+        while ( gameThread != null) {
+            currentTime = System.nanoTime();
+            repaint();
+            if ( currentTime - lastUpdateTime > updateGameInterval ) {
+                update();
+                lastUpdateTime = currentTime;
+            }
+            if ( currentTime - lastFPSTime > updateFPSInterval ) {
+                FPS = (double) ( numFrames * Constants.NANO_SEC_PER_SEC ) / ( currentTime - lastFPSTime );
+                System.out.format("FPS: [%.1f]\r", FPS);
+                numFrames   = 0;
+                lastFPSTime = currentTime;
+            }
+            numFrames++;
+        }
+
+    }
+
+    protected void startGameThread() {
+        gameThread = new Thread( this );
+        gameThread.start();
+    }
+
+    protected void paintComponent ( final Graphics graphics ) {
+        super.paintComponent( graphics );
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.dispose();
+    }
+
+    private void update() {
+
+    }
+}
