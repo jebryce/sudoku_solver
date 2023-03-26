a8ba6bfa08a1dcd5052541366053674364c52300
jebryce <116392465+jebryce@users.noreply.github.com>
2023-03-26 13:34:19 -1000

    created a Box container that can check if a tile's cord are in the box

---
+++ src/src/Box.java
@@ -0,0 +1,25 @@
+public class Box {
+    private final int boxXCord; // 0, 1, 2
+    private final int boxYCord; // 0, 1, 2
+
+    public Box( final int xCord, final int yCord ) {
+        boxXCord = xCord;
+        boxYCord = yCord;
+    }
+
+    public boolean isCordInBox( final int tileXCord, final int tileYCord ) {
+        if ( tileXCord < boxXCord * 3 ) {
+            return false;
+        }
+        if ( tileXCord >= ( boxXCord + 1 ) * 3 ) {
+            return false;
+        }
+        if ( tileYCord < boxYCord * 3 ) {
+            return false;
+        }
+        if ( tileYCord >= ( boxYCord + 1 ) * 3 ) {
+            return false;
+        }
+        return true;
+    }
+}
