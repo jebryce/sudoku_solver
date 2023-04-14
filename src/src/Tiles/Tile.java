3efe82560eaf14ca8e57bcff65b7f8fb1b9e2cc9
jebryce <116392465+jebryce@users.noreply.github.com>
2023-04-14 11:51:55 -1000

    Split part of Tile into PaintableTile

--- src/src/Tiles/Tile.java
+++ src/src/Tiles/Tile.java
@@ -1,24 +1,15 @@
 package Tiles;
 
-import Main.Colors;
 import Main.Constants;
 
-import java.awt.Graphics2D;
 import java.util.Arrays;
 
 public class Tile implements Cloneable {
-    private final int        xPos;
-    private final int        yPos;
-    private       int        value        = 0;
-    private       TileStatus tileStatus   = TileStatus.UNSET;
-    private       boolean[]  notes        = new boolean[Constants.NUM_TILES];
-    private       int        numNotes     = 0;
-    private       Tile[]     visibleTiles = new Tile[Constants.TOTAL_TILES];
-
-    public Tile( final int xCord, final int yCord ) {
-        xPos = xCord * Constants.TILE_SIZE;
-        yPos = yCord * Constants.TILE_SIZE;
-    }
+    protected int        value        = 0;
+    protected TileStatus tileStatus   = TileStatus.UNSET;
+    protected boolean[]  notes        = new boolean[Constants.NUM_TILES];
+    private   int        numNotes     = 0;
+    private   Tile[]     visibleTiles = new Tile[Constants.TOTAL_TILES];
 
     public void setFinalValue( final int finalValue ) {
         if ( valueIsInvalid( finalValue ) ) {
@@ -40,17 +31,6 @@ public class Tile implements Cloneable {
         unsetVisibleNotes( value );
     }
 
-    public void clear() {
-        if ( tileStatus == TileStatus.SET_FINAL ) {
-            return;
-        }
-        Arrays.fill( notes, false );
-        numNotes = 0;
-        value = 0;
-        tileStatus = TileStatus.UNSET;
-        clearVisibleDuplicates();
-    }
-
     public void setNote( final int newNote ) {
         if ( tileStatus != TileStatus.UNSET ) {
             return;
@@ -64,6 +44,17 @@ public class Tile implements Cloneable {
         }
     }
 
+    public void clear() {
+        if ( tileStatus == TileStatus.SET_FINAL ) {
+            return;
+        }
+        Arrays.fill( notes, false );
+        numNotes = 0;
+        value = 0;
+        tileStatus = TileStatus.UNSET;
+        clearVisibleDuplicates();
+    }
+
     public boolean[] getNotes() {
         return notes;
     }
@@ -74,52 +65,6 @@ public class Tile implements Cloneable {
 
     public TileStatus getTileStatus() {
         return tileStatus;
-    }
-
-    public void repaintBackground( final Graphics2D graphics2D ) {
-        graphics2D.fillRect( xPos, yPos, Constants.TILE_SIZE, Constants.TILE_SIZE );
-    }
-
-    public void repaintText( final Graphics2D graphics2D ) {
-        if ( tileStatus == TileStatus.UNSET ) {
-            return;
-        }
-        else if ( tileStatus == TileStatus.SET ) {
-            graphics2D.setColor( Colors.CHARCOAL_GRAY );
-        }
-        else if ( tileStatus == TileStatus.SET_FINAL) {
-            graphics2D.setColor( Colors.BLACK );
-        }
-        else if ( tileStatus == TileStatus.DUPLICATE ) {
-            graphics2D.setColor( Colors.CORAL_PINK );
-        }
-        graphics2D.drawString(
-                String.valueOf(value),
-                xPos + Constants.TILE_TEXT_X_OFFSET,
-                yPos + Constants.TILE_TEXT_Y_OFFSET
-        );
-    }
-
-    public void repaintNotes( final Graphics2D graphics2D ) {
-        if ( tileStatus != TileStatus.UNSET ) {
-            return;
-        }
-        graphics2D.setColor( Colors.CHARCOAL_GRAY );
-        float xOffset;
-        int yOffset;
-        int ct = 0;
-        for ( int i = 0; i < Constants.NUM_TILES; i++ ) {
-            if ( notes[i] ) {
-                xOffset = Constants.NOTES_X_OFFSET + ( ct%3 ) * ( Constants.NOTES_TEXT_WIDTH + Constants.NOTES_X_OFFSET );
-                yOffset = ( ct/3 + 1)  * ( Constants.NOTES_TEXT_HEIGHT + Constants.NOTES_Y_OFFSET );
-                graphics2D.drawString(
-                        String.valueOf(i+1),
-                        xPos + xOffset,
-                        yPos + yOffset
-                );
-                ct++;
-            }
-        }
     }
 
     public void setVisibleTile( final Tile newTile ) {
